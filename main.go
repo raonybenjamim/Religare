91ca827402964d8a8b63e7970e75b2ddbedab7e8
Raony Benjamim <raonybenjamim@gmail.com>
2024-06-18 21:17:38 -0300

    adding configuration flags

--- main.go
+++ main.go
@@ -1,24 +1,40 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"lazarus/converter"
 	"lazarus/interpreter"
+	"lazarus/models"
 )
 
 func main() {
+
+	generatorType := flag.String(
+		"generator-type",
+		models.GeneratorType.Wifi,
+		fmt.Sprintf("Define what will be the data generation behavior. Valid values are: %v", models.GeneratorType))
 
 	// THIS WAY WE READ SIGNALS FROM THE WIFI
 	// binaryChannel := make(chan models.Binary, models.ConverterChannelSize)
 
 	// go signalConverter.AddBinaryData(0.75, binaryChannel)
 
-	// READING RANDOM DATA
-	randomSignalGenerator := converter.NewRandomSignalGenerator(200)
+	flag.Parse()
 
-	go randomSignalGenerator.GenerateSignal()
+	var signalGenerator converter.SignalGenerator
+
+	switch generatorType {
+	case &models.GeneratorType.Random:
+		signalGenerator = converter.NewRandomSignalGenerator(200)
+
+	}
+
+	// Generate Signal
+	go signalGenerator.GenerateSignal()
 
 	channelReader := interpreter.ChannelReader{
-		Channel: randomSignalGenerator.Channel,
+		Channel: signalGenerator.GetChannel(),
 	}
 
 	channelReader.ReadChannel()
--- models/constants.go
+++ models/constants.go
@@ -4,3 +4,15 @@ const (
 	ConverterChannelSize = 100
 	ByteSize             = 8
 )
+
+type generatorType struct {
+	Random    string
+	Wifi      string
+	TextInput string
+}
+
+var GeneratorType = generatorType{
+	Random:    "random",
+	Wifi:      "wifi",
+	TextInput: "textInput",
+}
