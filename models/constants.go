a5c5be9346dc72bb14e2b6e7a36bf04f3cb5c26a
Raony <raonybenjamim@gmail.com>
2024-07-23 17:23:29 -0300

    Adding support for language chosing

--- models/constants.go
+++ models/constants.go
@@ -17,3 +17,23 @@ var GeneratorType = generatorType{
 	Wifi:      "wifi",
 	TextInput: "textInput",
 }
+
+type Language int
+
+const (
+	Portuguese Language = iota
+	English
+)
+
+func (l Language) String() string {
+	switch l {
+	case Portuguese:
+		return "Portuguese"
+
+	case English:
+		return "English"
+
+	default:
+		return "unknown"
+	}
+}
