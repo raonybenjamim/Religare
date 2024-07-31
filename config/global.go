d45d113f4d5f483fd85bdae5cf16e672acf358ed
Raony <raonybenjamim@gmail.com>
2024-07-31 20:40:53 -0300

    adding missing copyright

--- config/global.go
+++ config/global.go
@@ -1,3 +1,9 @@
+/*
+ * Religare - An Instrumental Trans Communication solution for communicating with paranormal entities
+ *
+ * Copyright (C) 2024 Raony Benjamim
+ * Check the LICENSE.md file for more information regarding the code license
+ */
 package config
 
 import (
