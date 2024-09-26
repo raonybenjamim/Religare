c9442c8739362d33b0320007098fe4d120c43cae
Raony <raonybenjamim@gmail.com>
2024-09-26 18:28:49 -0300

    Adjustments for main.go to run on configuration files

---
+++ models/executionConfig.go
@@ -0,0 +1,42 @@
+/*
+ * Religare - An Instrumental Trans Communication solution for communicating with paranormal entities
+ *
+ * Copyright (C) 2024 Raony Benjamim
+ * Check the LICENSE.md file for more information regarding the code license
+ */
+package models
+
+import (
+	"religare/customTypes"
+	"strings"
+)
+
+type ExecutionConfig struct {
+	Language         string `json:"language"`
+	GeneratorType    string `json:"generatorType"`
+	ValidationBypass bool   `json:"validationBypass"`
+}
+
+func (e *ExecutionConfig) ParseLanguage() customTypes.Language {
+	switch strings.ToLower(e.Language) {
+	case "pt-br":
+		return customTypes.Portuguese
+	case "en":
+		return customTypes.English
+	default:
+		return customTypes.English
+	}
+}
+
+func (e *ExecutionConfig) ParseGeneratorType() customTypes.GeneratorType {
+	switch strings.ToLower(e.GeneratorType) {
+	case "wifisignal":
+		return customTypes.WifiGeneratorType
+	case "random":
+		return customTypes.RandomGeneratorType
+	case "textinput":
+		return customTypes.TextInputGeneratorType
+	default:
+		return customTypes.WifiGeneratorType
+	}
+}
